b2beeaf9291e3f9adca5555422b51f9ca78de9ad
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-04-02 22:43:44 +1200

    click button action add counts

--- GUI.java
+++ GUI.java
@@ -1,20 +1,37 @@
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-class GUI {
+class GUI implements ActionListener {
+
+  private int counts = 0;
+  private JLabel label;
+  private JPanel panel;
+  private JFrame frame;
+  private JButton button;
 
   public GUI() {
-    JFrame frame = new JFrame();
+    frame = new JFrame();
 
-    JPanel panel = new JPanel();
+    panel = new JPanel();
+
+    button = new JButton("Click me");
+    button.addActionListener(this);
+
+    label = new JLabel("Number of Clicks: 0");
 
     panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-    panel.setLayout(new GridLayout());
+    panel.setLayout(new GridLayout(0, 1));
+    panel.add(button);
+    panel.add(label);
 
     frame.add(panel, BorderLayout.CENTER);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,6 +43,12 @@ class GUI {
 
   public static void main(String[] args) {
     new GUI();
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    this.counts++;
+    label.setText("Number of Clicks: " + this.counts);
   }
 
 }
